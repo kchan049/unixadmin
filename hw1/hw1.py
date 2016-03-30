70c09d29382300827e56cc0db399865fb0326a19
Kenneth Chan <kchan049@ucr.edu>
2016-03-30 13:35:33 -0700

    Update hw1.py

--- hw1/hw1.py
+++ hw1/hw1.py
@@ -1,7 +1,8 @@
 #!/usr/local/bin/python
 #usage: python cat.py filename
+from __future__ import print_function
 import sys
 for count in range(1, len(sys.argv)):
 	f = open(sys.argv[count])
-	print f.read()
+	print (f.read(), end = "")
 	f.close()
