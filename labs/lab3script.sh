63cdc7bd4df324e296820d437bf588b58bfd520b
Kenneth Chan <KennethChan@Kenneths-MBP.attlocal.net>
2016-04-14 13:56:22 -0700

    lab3 complete

--- labs/lab3script.sh
+++ labs/lab3script.sh
@@ -2,22 +2,22 @@
 #question 1
 find /sbin /bin /usr/bin /usr/sbin -user root -perm -4000
 #Setuid 0 or root means that when you execute or run a program, it is as the root user ran it. When you run something with such high permissions, it could potentially do anything to your system.
-read -p "Hit any key to continue."
+read -p "Hit enter to continue."
 #question 2
 find /var -cmin -20
-read -p "Hit any key to continue."
+read -p "Hit enter to continue."
 #question 3
 find /var -type f -size 0
-read -p "Hit any key to continue."
+read -p "Hit enter to continue."
 #question 4
 find /var -not -type f -not -type d -ls
-read -p "Hit any key to continue."
+read -p "Hit enter to continue."
 #question 5
 find /home -type d -not -user root -exec chmod 711 {} \;
-read -p "Hit any key to continue."
+read -p "Hit enter to continue."
 #question 6
 find /home -type f -not -user root -exec chmod 755 {} \;
-read -p "Hit any key to continue."
+read -p "Hit enter to continue."
 #question 7
 find /etc -mtime -5
 
