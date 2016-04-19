0e85bb803f326f0f2acdbf53b7979aaf941c383d
Kenneth Chan <kchan049@ucr.edu>
2016-04-18 19:49:21 -0700

    hw3

--- hw3/one.pl
+++ hw3/one.pl
@@ -1,8 +1,26 @@
-#!/bin/perl
-
+#!/usr/bin/perl
 open(FILE, $ARGV[0]);
-
+$post="reject";
+$amavis="amavis";
+my $postcount=0;
+my $acount=0;
 while(<FILE>){
-	@line = $_;
-	printf("line %2d: %s", $c++, $line);
+	chomp;
+	($h, $m, $s) = split("\:");
+	#print "$m\n";
+	($month, $numb, $user, $dir, $num, $blah,$stat )=split("\ ");
+	#print $stat;
+	print "$month $numb";
+	#for(my $i=0; $i<60; $i++)
+	#{
+	#	print "$month";
+	#	while($i=$m)
+	#	{
+	#		if(index($stat,$post) != -1 )
+	#		{
+	#			$postcount++;
+	#		}
+	#	}
+	#	print "$postcount\n";
+	#}
 }
