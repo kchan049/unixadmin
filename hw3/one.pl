f5380ce4e779fc3d9aaa9d5a3fabfcc746718e27
Kenneth Chan <kchan049@ucr.edu>
2016-04-19 18:04:22 -0700

    finished ex1 and 2

--- hw3/one.pl
+++ hw3/one.pl
@@ -1,26 +1,46 @@
-#!/usr/bin/perl
-open(FILE, $ARGV[0]);
-$post="reject";
-$amavis="amavis";
-my $postcount=0;
-my $acount=0;
-while(<FILE>){
-	chomp;
-	($h, $m, $s) = split("\:");
-	#print "$m\n";
-	($month, $numb, $user, $dir, $num, $blah,$stat )=split("\ ");
-	#print $stat;
-	print "$month $numb";
-	#for(my $i=0; $i<60; $i++)
-	#{
-	#	print "$month";
-	#	while($i=$m)
-	#	{
-	#		if(index($stat,$post) != -1 )
-	#		{
-	#			$postcount++;
-	#		}
-	#	}
-	#	print "$postcount\n";
-	#}
+#!/usr/bin/env perl
+use strict;
+use warnings;
+
+my $logfile = $ARGV[0];
+
+open (my $LOG, "<", $logfile) or die "can't open " . $logfile;
+open (my $INFO, ">", "hourlyInfo") or die "can't open hourlyInfo";
+
+my $time;
+my $prev_time = "o.o";
+my $rejects = 0;
+my $quarantines = 0;
+while (my $line = <$LOG>) {
+	if ($line =~ /^(.*?):(\S+):/) {
+		my $hour = $1;
+		my $minute = $2;
+		$time = $hour . ":" . $minute;
+		if ($time eq $prev_time) {
+			if (index($line, "reject") != -1) {
+				++$rejects;
+
+			}
+			if (index($line, "quarantine") != -1) {
+				++$quarantines;
+			}
+		}
+		else {
+			if ($prev_time ne "o.o")
+			{
+				print $INFO $prev_time . "[postfix rejects:" . 
+				$rejects . "] [amavis quarantines:" .
+				$quarantines . "]\n";
+			}
+			$rejects = 0;
+			$quarantines = 0;
+		}
+		$prev_time = $time;
+	}
 }
+	print $INFO $prev_time . "[postfix rejects:" . 
+				$rejects . "] [amavis quarantines:" .
+				$quarantines . "]\n";
+
+close $LOG;
+close $INFO;
---
+++ hw3/two.pl
@@ -0,0 +1,43 @@
+#!/usr/bin/env perl
+use warnings;
+use List::Util 'sum';
+
+my $logfile = "maillog";
+open ($log, "<", $logfile) or die("Cannot open file " . $logfile);
+
+my %known;
+my %unknown;
+while (my $line = <$log>){
+  if ($line =~ /connect from (.*?)\[(\S+\.\S+\.\S+\.\S+)\]/){
+    my $status = $1;
+	my $ip = $2;
+	if (index($status ,"unknown") != -1){
+      if (!exists $unknown{$ip}) { 
+	  	$unknown{$ip} = 1; 
+	  }
+      else  { 
+	  	$unknown{$ip} = $unknown{$ip}+1; 
+	  }
+    }
+	else{
+      if (!exists $known{$ip}) { 
+	  	$known{$ip} = 1; 
+	  }
+      else  { 
+	  	$known{$ip} = $known{$ip}+1; 
+	  }
+    }
+
+  }
+}
+
+#sum number of values in the hash
+$k_sum = sum values %known;
+$max_known_ip = ((sort { $known{$b} <=> $known{$a} } keys %known)[0]);
+print "Total Known connection: " . $k_sum . " – [" . $max_known_ip . "] accounts for " . $known{$max_known_ip} . " connections\n";
+
+$u_sum = sum values %unknown;
+$max_unknown_ip = ((sort { $unknown{$b} <=> $unknown{$a} } keys %unknown)[0]);
+print "Total Unknown connection: " . $u_sum . " – [" . $max_unknown_ip . "] accounts for " . $unknown{$max_unknown_ip} . " connections\n";
+
+close $log;
