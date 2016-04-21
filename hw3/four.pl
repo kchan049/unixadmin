445b589513c412494fdb9ea1d253d4cbb44a9b0d
Kenneth Chan <kchan049@ucr.edu>
2016-04-20 20:14:41 -0700

    fixed based on address and not entire fire for part 4

--- hw3/four.pl
+++ hw3/four.pl
@@ -10,49 +10,36 @@ open(my $LOG, "<", $logfile) or die "cannot open" . $logfile;
 
 while(<$LOG>)
 {
-	if ($_ =~ /blocked using dnsbl\.sorbs\.net/)
-	{
-		$blockcount++;
-	}
-}
-print $blockcount. " messages rejected\n";
-close($LOG);
-open($LOG, "<", $logfile);
-while(<$LOG>)
-{
-	if ($_ =~ /\[(\S+\.\S+\.\S+\.\S+)\]/)
+	if ($_ =~ /Client host \[(\S+\.\S+\.\S+\.\S+)\] blocked using dnsbl\.sorbs\.net/)
 	{
 		my $ip = $1;
-		if( !exists $ip_count{$ip})
+		$blockcount++;
+		if (!exists $ip_count{$ip})
 		{
 			$ip_count{$ip}++;
 		}
+			
+		if ($_ =~ /from\=\<(\S+)\>/)
+		{
+			my $email = $1;
+			if( !exists $from{$email})
+			{
+				$from{$email}++;
+			}
+		}
+		if ($_ =~ /to\=\<(\S+)\>/)
+		{
+			my $email1 = $1;
+			if( !exists $to{$email1})
+			{
+				$to{$email1}++;
+			}
+	}		
 	}
 }
+print $blockcount. " messages rejected\n";
 $ip_sum = sum values %ip_count;
 print $ip_sum . " unique IP's\n";
-close($LOG);
-open($LOG, "<", $logfile);
-while(<$LOG>)
-{
-	if ($_ =~ /from\=\<(\S+)\>/)
-	{
-		my $email = $1;
-		if( !exists $from{$email})
-		{
-			$from{$email}++;
-		}
-	}
-	if ($_ =~ /to\=\<(\S+)\>/)
-	{
-		my $email1 = $1;
-		if( !exists $to{$email1})
-		{
-			$to{$email1}++;
-		}
-	}
-	
-}
 $tosum = sum values %to;
 $fromsum = sum values %from;
 print $fromsum . " unique from addresses\n";
