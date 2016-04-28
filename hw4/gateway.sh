1312f4b802383256e47c3d03c1d63325bc110f5f
Kenneth Chan <kchan049@ucr.edu>
2016-04-28 14:14:46 -0700

    Added files via upload

--- hw4/gateway.sh
+++ hw4/gateway.sh
@@ -1,10 +1,10 @@
-#!bin/bash 
+#!/bin/bash 
 
 IP=$(/sbin/ip route | awk '/default/ { print $3 }')
 
 DEVICE=$(/sbin/ip route | awk '/default/ { print $5 }')
 
-NETMASK=$(/sbin/ifconfig eth0 | awk '/Mask:/{ print $4;} ')
+NETMASK=$(/sbin/ifconfig $DEVICE | awk '/Mask:/{ print $4;} ')
 
 
 echo "gateway: $IP Net$NETMASK device: $DEVICE "
--- hw4/netmask.py
+++ hw4/netmask.py
@@ -12,33 +12,33 @@ address = (2 ** bit)
 
 usable = 1
 
-print "address: {}".format(address),
+print "address: " + str(address),
 
 if int(sys.argv[1]) == 32:
 	usable = 1
 	ip = 256-address
-	print "usable: {}".format(usable),
-	print "netmask: 255.255.255.{}".format(ip)
+	print "usable: "+str(usable),
+	print "netmask: 255.255.255."+str(ip)
 elif int(sys.argv[1]) >= 24 and int(sys.argv[1]) <= 31:
 	usable = address -2
 	ip = 256-address
-	print "usable: {}".format(usable),
-	print "netmask: 255.255.255.{}".format(ip)
+	print "usable: "+str(usable),
+	print "netmask: 255.255.255."+str(ip)
 elif int(sys.argv[1]) >= 16 and int(sys.argv[1]) <=23:
 	usable = address -2
 	ip = 256-(2 ** (bit-8))	
-	print "usable: {}".format(usable),
-	print "netmask: 255.255.{}.0".format(ip)
+	print "usable: "+ str(usable),
+	print "netmask: 255.255." + str(ip) + ".0"
 elif int(sys.argv[1]) >= 8 and int(sys.argv[1]) <=15:
 	usable = address -2
 	ip = 256-(2 ** (bit-16))
-	print "usable: {}".format(usable),
-	print "netmask: 255.{}.0.0".format(ip)
+	print "usable: "+str(usable),
+	print "netmask: 255." + str(ip) + ".0.0"
 elif int(sys.argv[1]) >= 0 and int(sys.argv[1]) <=7:
 	usable = address -2
 	ip = 256-(2 ** (bit-24))
-	print "usable: {}".format(usable),
-	print "netmask: {}.0.0.0".format(ip)
+	print "usable: "+str(usable),
+	print "netmask: " +str(ip)+ ".0.0.0"
 
 
 
